50a2197869feca10af3d15f62eaa78939fdad773
Brittany Mazza <bmazza@techempower.com>
2015-01-22 13:16:30 -0800

    Make tests run, all tests are currently failing.
    - Remove bash_profile.sh
    - composer.phar wasn't being referenced correctly in install.

--- frameworks/PHP/php-silica/setup.sh
+++ frameworks/PHP/php-silica/setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+export PHP_HOME=${IROOT}/php-5.5.17
+export PHP_FPM=$PHP_HOME/sbin/php-fpm
+export NGINX_HOME=${IROOT}/nginx
+
 sed -i 's|192.168.100.102|'"${DBHOST}"'|g' web/app.php
 sed -i 's|"./FrameworkBenchmarks/php-silica|"'"${TROOT}"'|g' deploy/php-silica
 sed -i 's|Directory .*/FrameworkBenchmarks/php-silica|Directory '"${TROOT}"'|g' deploy/php-silica
@@ -8,6 +12,6 @@ sed -i 's|/usr/local/nginx/|'"${IROOT}"'/nginx/|g' deploy/nginx.conf
 
 export PATH="$COMPOSER_HOME:$PHP_HOME/bin:$PHP_HOME/sbin:$PATH"
 
-composer.phar install
+$IROOT/composer.phar install
 $PHP_FPM --fpm-config $FWROOT/config/php-fpm.conf -g $TROOT/deploy/php-fpm.pid
 $NGINX_HOME/sbin/nginx -c $TROOT/deploy/nginx.conf
