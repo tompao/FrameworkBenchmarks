925f8681ed802bfd1e6183559fce9f668aa29a92
Brittany Mazza <bmazza@techempower.com>
2015-01-22 13:47:05 -0800

    Install composer.phar only in install. Clean up for consitency.

--- frameworks/PHP/php-silica/install.sh
+++ frameworks/PHP/php-silica/install.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 export PHP_HOME=${IROOT}/php-5.5.17
-
-export PHP_FPM=$PHP_HOME/sbin/php-fpm
-
+export PHP_FPM=${PHP_HOME}/sbin/php-fpm
 export NGINX_HOME=${IROOT}/nginx
 
 fw_depends php composer nginx
 
-${PHP_HOME}/bin/php $IROOT/composer.phar install \
-  --no-interaction --working-dir $TROOT \
+${PHP_HOME}/bin/php ${IROOT}/composer.phar install \
+  --no-interaction --working-dir ${TROOT} \
   --no-progress --optimize-autoloader 
--- frameworks/PHP/php-silica/setup.sh
+++ frameworks/PHP/php-silica/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export PHP_HOME=${IROOT}/php-5.5.17
-export PHP_FPM=$PHP_HOME/sbin/php-fpm
+export PHP_FPM=${PHP_HOME}/sbin/php-fpm
 export NGINX_HOME=${IROOT}/nginx
 
 sed -i 's|192.168.100.102|'"${DBHOST}"'|g' web/app.php
@@ -10,8 +10,7 @@ sed -i 's|Directory .*/FrameworkBenchmarks/php-silica|Directory '"${TROOT}"'|g' 
 sed -i 's|root .*/FrameworkBenchmarks/php-silica|root '"${TROOT}"'|g' deploy/nginx.conf
 sed -i 's|/usr/local/nginx/|'"${IROOT}"'/nginx/|g' deploy/nginx.conf
 
-export PATH="$COMPOSER_HOME:$PHP_HOME/bin:$PHP_HOME/sbin:$PATH"
+export PATH="${COMPOSER_HOME}:${PHP_HOME}/bin:${PHP_HOME}/sbin:${PATH}"
 
-$IROOT/composer.phar install
-$PHP_FPM --fpm-config $FWROOT/config/php-fpm.conf -g $TROOT/deploy/php-fpm.pid
-$NGINX_HOME/sbin/nginx -c $TROOT/deploy/nginx.conf
+${PHP_FPM} --fpm-config ${FWROOT}/config/php-fpm.conf -g ${TROOT}/deploy/php-fpm.pid
+${NGINX_HOME}/sbin/nginx -c ${TROOT}/deploy/nginx.conf
